f0a85febbaf1d588041a6982fe5bf7efa307cd15
Ange3108 <aguatemala40593@ufide.ac.cr>
2025-02-06 15:55:33 -0600

    atributos clase cuerpo celeste

--- SistemaSolar/src/sistemasolar/CuerpoCeleste.java
+++ SistemaSolar/src/sistemasolar/CuerpoCeleste.java
@@ -4,5 +4,10 @@ package sistemasolar;
 public abstract class CuerpoCeleste {
     String orbita="";
     double gravedad=0;
-    double densidad
+    String nombre="";
+    double tamano=0;
+    double distanciaSol=0;
+    int numLuna=0;
+    
+    
 }
