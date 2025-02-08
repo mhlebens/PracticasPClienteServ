c7b5ae911d6fb6d1b6b418c53ee6d0a7bbe13068
Pablo Arias <parias40359@ufide.ac.cr>
2025-02-07 23:16:40 -0600

    listeishon

--- SistemaSolar/src/sistemasolar/PlanetaRocoso.java
+++ SistemaSolar/src/sistemasolar/PlanetaRocoso.java
@@ -1,6 +1,39 @@
 package sistemasolar;
 
+public class PlanetaRocoso extends CuerpoCeleste implements Planeta {
 
-public class PlanetaRocoso {
+    private int numLunas;
+
+    public PlanetaRocoso(int numLunas, String nombre, double tamano, String orbita, double distanciaSol) {
+        super(nombre, tamano, orbita, distanciaSol);
+        this.numLunas = numLunas;
+    }
+
+    public int getNumLunas() {
+        return numLunas;
+    }
+
+    public void setNumLunas() {
+        this.numLunas = numLunas;
+    }
+    
+    // Calcula el tiempo que tarda el planeta en dar una vuelta alrededor del Sol (año orbital)
+    @Override
+    public double calcularAños() {
+        return Math.sqrt(Math.pow(getDistanciaSol(), 3));
+    }
+        
+    // Calcula la gravedad del planeta basada en su tamaño (factor de ajuste para planetas rocosos)
+    @Override
+    public double getGravedad() {
+        return (getTamano() * 9.8) / 6371;
+    }
+
+    // Representa en forma de cadena el objeto, mostrando sus atributos principales
+    @Override
+    public String toString(){
+        return "Planeta rocoso: " + super.toString() + "\n Cantidad de lunas: " +
+                this.getNumLunas() + "\n Año orbital: " + this.calcularAños();
+    }
     
 }
