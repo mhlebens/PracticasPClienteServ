2dcb81f0682daf7c184e1cc716bd957bbd98f2a4
Pablo Arias <parias40359@ufide.ac.cr>
2025-02-07 23:15:49 -0600

    ingresé mi contraseña y aún así no me dejó actualizarlo desde netbeans xddd

--- SistemaSolar/src/sistemasolar/PlanetaGaseoso.java
+++ SistemaSolar/src/sistemasolar/PlanetaGaseoso.java
@@ -1,6 +1,41 @@
 package sistemasolar;
 
+public class PlanetaGaseoso extends CuerpoCeleste implements Planeta {
 
-public class PlanetaGaseoso {
-   
+    private int numLunas;
+
+    
+    //Se aplican un 90% de los mismos métodos del planeta rocoso.
+    public PlanetaGaseoso(int numLunas, String nombre, double tamano, String orbita, double distanciaSol) {
+        super(nombre, tamano, orbita, distanciaSol);
+        this.numLunas = numLunas;
+    }
+
+    public int getNumLunas() {
+        return numLunas;
+    }
+
+    public void setNumLunas() {
+        this.numLunas = numLunas;
+    }
+    
+    // Calcula el tiempo que tarda el planeta en dar una vuelta alrededor del Sol (año orbital)
+    @Override
+    public double calcularAños() {
+        return Math.sqrt(Math.pow(getDistanciaSol(), 3));
+    }
+        
+    // Calcula la gravedad del planeta basada en su tamaño (factor de ajuste para planetas rocosos)
+    @Override
+    public double getGravedad() {
+        return (getTamano() * 24.79) / 69911;
+    }
+
+    // Representa en forma de cadena el objeto, mostrando sus atributos principales
+    @Override
+    public String toString(){
+        return "Planeta rocoso: " + super.toString() + "\n Cantidad de lunas: " +
+                this.getNumLunas() + "\n Año orbital: " + this.calcularAños();
+    }
+    
 }
