a20769edb06935c238535559abbac4e854c026b6
Pablo Arias <parias40359@ufide.ac.cr>
2025-02-09 20:45:48 -0600

    Planetoide en su versión final

--- SistemaSolar/src/sistemasolar/Planetoide.java
+++ SistemaSolar/src/sistemasolar/Planetoide.java
@@ -1,6 +1,33 @@
 package sistemasolar;
 
+public abstract class Planetoide extends CuerpoCeleste implements Planeta {
 
-public class Planetoide extends CuerpoCeleste implements Planeta{
-   
+    private int numLunas;
+
+    public Planetoide(int numLunas, String nombre, double tamano, String orbita, double distanciaSol) {
+        super(nombre, tamano, orbita, distanciaSol);
+        this.numLunas = numLunas;
+    }
+
+    public int getNumLunas() {
+        return numLunas;
+    }
+
+    public void setNumLunas(int numLunas) {
+        this.numLunas = numLunas;
+    }
+
+    // Método para calcular el año orbital (en base a la 3ra Ley de Kepler)
+    @Override
+    public double calcularAños() {
+        return Math.sqrt(Math.pow(getDistanciaSol(), 3));
+    }
+
+    // Representa en forma de cadena el objeto, mostrando sus atributos principales
+    @Override
+    public String toString() {
+        return super.toString() + "\n Cantidad de lunas: "
+                + this.getNumLunas() + "\n Año orbital: " + this.calcularAños();
+    }
+
 }
