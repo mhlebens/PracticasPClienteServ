cb3a36cf98d152256a585d98f995ccdfd5ba7ebc
Ange3108 <aguatemala40593@ufide.ac.cr>
2025-02-10 10:21:53 -0600

    una cosita ahí

--- SistemaSolar/src/sistemasolar/Principal.java
+++ SistemaSolar/src/sistemasolar/Principal.java
@@ -6,15 +6,16 @@ public class Principal {
         
         //creación del array para asignar valores a los planetas
         CuerpoCeleste planet [] = new CuerpoCeleste [4];
-        planet [0] = new PlanetaGaseoso (95, "Júpiter", 69911, "778 millones de kilómetros", 780000000);
-        planet [1] = new PlanetaGaseoso (146, "Saturno", 58232, "1418 millones de kilómetros", 1430000000);
-        planet [2] = new PlanetaRocoso (0, "Mercurio", 2439.7, "46 millones de kilómetros", 46001200);
-        planet [3] = new PlanetaRocoso (2, "Marte", 3389.5, "249 millones de kilómetros", 228000000);
+        planet [0] = new PlanetaGaseoso (95, "Júpiter", "planeta", 69911, "778 millones de kilómetros", 780000000);
+        planet [1] = new PlanetaGaseoso (146, "Saturno","planeta" , 58232, "1418 millones de kilómetros", 1430000000);
+        planet [2] = new PlanetaRocoso (0, "Mercurio", "planeta", 2439.7, "46 millones de kilómetros", 46001200);
+        planet [3] = new PlanetaRocoso (2, "Marte", "planeta",3389.5, "249 millones de kilómetros", 228000000);
         
         for (int i = 0; i < 10; i++) {
-            System.out.println("Datos de los planetas"+planet[i]+planet.toString()+"\n");
+            System.out.println("Datos de los planetas\n"+planet[i]+"\n");
         }
         
+       
     }
     
 }
